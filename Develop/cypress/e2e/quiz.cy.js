7ea590d5a083f3f7c440a27ca6757f4b954e59dd
naiknetra <naiknetra5@gmail.com>
2025-01-28 16:24:58 -0500

    updated e2e file

--- Develop/cypress/e2e/quiz.cy.js
+++ Develop/cypress/e2e/quiz.cy.js
@@ -8,5 +8,23 @@ describe('Quiz Test Suite', () => {
     cy.get('.btn-primary').should('contain','1');
  })
 })
+   it('Clicking the Next button should show the next question', () => {
+      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').should('contain','2');
+   })
+   it('Clicking the Previous button should show the previous question', () => {
+      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').should('contain','1');
+   })
+   it('Clicking the Finish button should show the final score', () => {
+      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').should('contain','1');
+   })
+   it('Clicking the Play Again button should reset the quiz', () => {
+      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').should('contain','1');
+   })
+
+   
 
 

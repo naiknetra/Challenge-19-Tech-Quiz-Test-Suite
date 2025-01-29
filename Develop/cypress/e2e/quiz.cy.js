0e9e256de6e0f45f603e4df90381daa299f95522
naiknetra <naiknetra5@gmail.com>
2025-01-29 10:05:09 -0500

    updated cypress file

--- Develop/cypress/e2e/quiz.cy.js
+++ Develop/cypress/e2e/quiz.cy.js
@@ -8,11 +8,11 @@ describe('Quiz Test Suite', () => {
     cy.get('.btn-primary').should('contain','1');
  })
 })
-   it('Clicking the Next button should show the next question', () => {
+   it('Clicking the first option button should select option one', () => {
       cy.get('.btn-primary').click();
       cy.get('.btn-primary').should('contain','2');
    })
-   it('Clicking the Previous button should show the previous question', () => {
+   it('Clicking the second option button should select the previous question', () => {
       cy.get('.btn-primary').click();
       cy.get('.btn-primary').should('contain','1');
    })
